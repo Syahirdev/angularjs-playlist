47de526eeb49c373a186a91ec62c797a875ccbc2
Syahir Dev <888syahir@gmail.com>
2020-09-10 20:33:42 +0800

    adding filters to person data
    piping using |

--- app/app.js
+++ app/app.js
@@ -9,7 +9,27 @@ myPersonApp.run(function () {});
 myPersonApp.controller("PersonController", [
 	"$scope",
 	function ($scope) {
-		$scope.message = "Hello World";
-		$scope.persons = ["Syazmi", "Syakira", "Syahir", "Sameon"];
+		$scope.persons = [
+			{
+				name: "Syahir",
+				color: "green",
+				weight: 50,
+			},
+			{
+				name: "Syazmi",
+				color: "blue",
+				weight: 45,
+			},
+			{
+				name: "Syakira",
+				color: "yellow",
+				weight: 47,
+			},
+			{
+				name: "Sameon",
+				color: "red",
+				weight: 69,
+			},
+		];
 	},
 ]);
